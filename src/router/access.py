63c787be4474344b7a9e669c96e6965b764154e3
rukaSUKII <sobonukasz@gmail.com>
2024-11-14 18:52:22 +0100

    Implement register and login endpoints for app accounts. Fix JWTs creation.

--- src/router/access.py
+++ src/router/access.py
@@ -1,21 +1,71 @@
 from fastapi import APIRouter, HTTPException
+from grpc import RpcError
+import json
+from pydantic import BaseModel
+
 from src.connections import channel
 from src.utils.auth import create_jwt_token
 from user import user_pb2, user_pb2_grpc
 
+class Credentials(BaseModel):
+    email:str
+    password:str
+
+class RegisterData(BaseModel):
+    username:str
+    password:str
+    email:str
+    name:str | None = None
+    surname:str | None = None
+    street:str | None = None
+    building:str | None = None
+    city:str | None = None
+    postal_code:str | None = None
+    country:str | None = None
+
 router = APIRouter()
 
-@router.post("/login/")
-def login(email: str, password: str):
-    if email is None or password is None:
-         raise HTTPException(status_code=400, detail="Invalid credentials")
+@router.post("/login")
+def login(credentials:Credentials):
 
-    credentials = user_pb2.AuthUser(email=email, password=password)
-    stub = user_pb2_grpc.UserStub(channel)
-    response:user_pb2.AuthResponse = stub.Authenticate(credentials)
-    if response and response.success:
+    credentials = user_pb2.AuthUser(**credentials.dict())
+
+    try:
+        stub = user_pb2_grpc.UserStub(channel)
+        response: user_pb2.AuthResponse = stub.Authenticate(credentials)
+    except RpcError as e:
+        print("gRPC error details:", e.details())
+        print("gRPC status code:", e.code())
+        raise HTTPException(status_code=500, detail=f"gRPC call failed: {e.details()}")
+
+    if response.success:
         jwt_data = {"id": response.id, "email": response.email, "login": response.username}
-        jwt_token = 'Bearer ' + create_jwt_token(jwt_data)
+        jwt_token = create_jwt_token(jwt_data)
 
+        endpoint_response = {"accessToken": jwt_token,
+                             "authScheme": 'Bearer',
+                             "email": response.email,
+                             "username": response.username}
+
+        return endpoint_response
+
+    raise HTTPException(status_code=400, detail="Invalid credentials")
+
+@router.post("/register")
+def register_user(registerData:RegisterData):
+
+    data_for_grpc = user_pb2.RegUser(**registerData.dict())
+
+    try:
+        stub = user_pb2_grpc.UserStub(channel)
+        response: user_pb2.RegResponse = stub.Register(data_for_grpc)
+    except RpcError as e:
+        print("gRPC error details:", e.details())
+        print("gRPC status code:", e.code())
+        raise HTTPException(status_code=500, detail=f"gRPC call failed: {e.details()}")
+
+    print(response.success)
+
+    return {"account_created": response.success}
 
 
--- src/utils/auth.py
+++ src/utils/auth.py
@@ -1,24 +1,22 @@
 import jwt
 from datetime import datetime, timedelta, UTC
-from dotenv import load_dotenv
-from os import getenv
 
-load_dotenv("Frontend_API/.env")
+from src.utils import JWT_SECRET_KEY
 
-SECRET_KEY = getenv("SECRET_KEY")
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 10
 
 
 def create_jwt_token(data: dict):
-    data['iat'] = datetime.now(UTC)
-    data['exp'] = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    data['iat'] = datetime.now(UTC).isoformat()
+    data['exp'] = (datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)).isoformat()
+    return jwt.encode(data, JWT_SECRET_KEY, algorithm=ALGORITHM)
 
 
 def verify_jwt_token(token: str):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
         return payload
     except jwt.ExpiredSignatureError | jwt.InvalidTokenError:
         return None
