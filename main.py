6c8667c11ed8be6194a9b5266591c3d9858aafe5
rukaSUKII <sobonukasz@gmail.com>
2024-11-14 18:50:11 +0100

    Remove root endpoint

--- main.py
+++ main.py
@@ -2,21 +2,11 @@ from fastapi import FastAPI
 from src.router import access
 import uvicorn
 
-from src.connections.connections import open_db_manager_connection
-
-#db_channel = open_db_manager_connection()
-
 app = FastAPI()
 
-app.include_router(access.router)
-
-
-@app.get("/")
-async def root():
-    return {f'Hello World" + {12}'}
-
+app.include_router(access.router, prefix="/api")
 
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, )
