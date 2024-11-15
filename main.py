dacc7d981d99cdc9a0ebbef91832ec9c56b96c9b
rukaSUKII <sobonukasz@gmail.com>
2024-11-15 20:36:56 +0100

    Change route for auth endpoints

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
-from src.router import access
+from fastapi.responses import JSONResponse
 import uvicorn
+
+from src.router import access
+from src.exceptions.GrpcException import GrpcException
 
 app = FastAPI()
 
-app.include_router(access.router, prefix="/api")
+app.include_router(access.router, prefix="/api/auth")
+
+
+@app.exception_handler(GrpcException)
+async def grpc_exception_handler(request, exc: GrpcException):
+    return JSONResponse(
+        status_code = 500,
+        content = {"detail":str(exc)}
+    )
 
 
 
