e5a61aca7083249a5e2f2f7178d1ee2edb319951
rukaSUKII <sobonukasz@gmail.com>
2024-11-15 20:37:58 +0100

    Remove custom gRPC exception

--- main.py
+++ main.py
@@ -3,19 +3,11 @@ from fastapi.responses import JSONResponse
 import uvicorn
 
 from src.router import access
-from src.exceptions.GrpcException import GrpcException
+
 
 app = FastAPI()
 
 app.include_router(access.router, prefix="/api/auth")
-
-
-@app.exception_handler(GrpcException)
-async def grpc_exception_handler(request, exc: GrpcException):
-    return JSONResponse(
-        status_code = 500,
-        content = {"detail":str(exc)}
-    )
 
 
 
