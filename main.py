f3c88740b91184431f84cf5117e9a971b43ba04e
rukaSUKII <sobonukasz@gmail.com>
2024-11-15 21:20:47 +0100

    Fix CORS

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from src.router import access
 
 
 app = FastAPI()
+
+origins = [
+    "Localhost",
+    "Localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(access.router, prefix="/api/auth")
 
