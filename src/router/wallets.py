b492fe14247058e0b14041cdbdc19ef72e57f76e
rukaSUKII <sobonukasz@gmail.com>
2024-11-18 21:24:03 +0100

    Add endpoints to apply operations on wallets

--- src/router/wallets.py
+++ src/router/wallets.py
@@ -1,34 +1,36 @@
-from fastapi import APIRouter, HTTPException
+from google.protobuf.json_format import MessageToDict
+from fastapi import APIRouter, HTTPException, Request
 from grpc import RpcError
 from pydantic import BaseModel
 
 from src.connections import channel
 from user import user_detail_pb2
 from wallet import wallet_pb2, wallet_pb2_grpc
+from src.utils.auth import verify_user
 
 
 class WalletCreationData(BaseModel):
-    id: str | None = None
+    id: str | None = ""
     currency: str
-    value: str | None = None
+    value: str | None = "0"
     user_id: str
 
 
 class WalletUpdateData(BaseModel):
-    id: str | None = None
-    currency: str
+    id: str
+    currency: str | None = ""
     value: str
-    user_id: str
+    user_id: str | None = ""
 
 
 class WalletsOwnerData(BaseModel):
     id: str
 
 
-wallets = APIRouter()
+wallets = APIRouter(tags=["Wallets"])
 
 
-@wallets.post("/create-wallet", responses={
+@wallets.post("/", responses={
     500: {
         "description": "Problems occurred inside the server",
         "content": {
@@ -45,28 +47,57 @@ wallets = APIRouter()
             }
         }
     },
-})
-def create_walllet(wallet_data: WalletCreationData):
-    if wallet_data.value is None:
-        wallet_data.value = "0"
+    401: {
+        "description": "Authorization failure",
+        "content": {
+            "application/json": {
+                "example": [{"detail": "no_authorization_header"},
+                            {"detail": "invalid_auth_scheme"},
+                            {"detail": "invalid_token"},
+                            {"detail": "expired_token"},
+                            {"detail": "unauthorized_user_for_method"}
+                            ]
+
+            }
+        }
+    },
+    200: {
+        "description": "Details of created wallet",
+        "content": {
+            "application/json": {
+                "example": {
+                    "id": "1",
+                    "currency": "PLN",
+                    "value": "250.32",
+                    "user_id": "2"
+                }
+            }
+        }
+    }
+}, description='Returns details about created or existing wallet')
+def create_wallet(wallet_data: WalletCreationData, request: Request):
+    auth_header = request.headers.get("Authorization")
+    jwt_payload = verify_user(auth_header)
+    if str(jwt_payload.get("id")) != wallet_data.user_id:
+        raise HTTPException(status_code=401, detail="unauthorized_user_for_method")
 
     wallet_message = wallet_pb2.Wallet(**wallet_data.dict())
 
     try:
         stub = wallet_pb2_grpc.WalletsStub(channel)
-        response: wallet_pb2.Wallet = stub.createWallet(wallet_message)
+        response = stub.createWallet(wallet_message)
     except RpcError as e:
         print("gRPC error details:", e.details())
         print("gRPC status code:", e.code())
         raise HTTPException(status_code=500, detail="internal_server_error")
 
-    if response.id is not None:
-        return response
+    if response.id != "":
+        return MessageToDict(response)
 
     raise HTTPException(status_code=400, detail="operation_failed")
 
 
-@wallets.post("update-wallet", responses={
+@wallets.put("/", responses={
     500: {
         "description": "Problems occurred inside the server",
         "content": {
@@ -83,8 +114,37 @@ def create_walllet(wallet_data: WalletCreationData):
             }
         }
     },
-})
-def update_wallet(update_wallet_data: WalletUpdateData):
+    401: {
+        "description": "Authorization failure",
+        "content": {
+            "application/json": {
+                "example": [{"detail": "no_authorization_header"},
+                            {"detail": "invalid_auth_scheme"},
+                            {"detail": "invalid_token"},
+                            {"detail": "expired_token"},
+                            {"detail": "unauthorized_user_for_method"}
+                            ]
+
+            }
+        }
+    },
+    200: {
+        "description": "Details of the wallet after update",
+        "content": {
+            "application/json": {
+                "example": {
+                    "id": "1",
+                    "currency": "PLN",
+                    "value": "250.32",
+                    "user_id": "2"
+                }
+            }
+        }
+    }
+}, description="Update of wallet. Fields id and value are obligatory.")
+def update_wallet(update_wallet_data: WalletUpdateData, request: Request):
+    get_wallet_by_id(update_wallet_data.id, request)
+
     data_for_update = wallet_pb2.Wallet(**update_wallet_data.dict())
 
     try:
@@ -95,13 +155,13 @@ def update_wallet(update_wallet_data: WalletUpdateData):
         print("gRPC status code:", e.code())
         raise HTTPException(status_code=500, detail="internal_server_error")
 
-    if response.id is not None:
-        return {"success": True, **response}
+    if response.id != "":
+        return MessageToDict(response)
 
     raise HTTPException(status_code=400, detail="operation_failed")
 
 
-@wallets.get("get-wallets", responses={
+@wallets.get("/", responses={
     500: {
         "description": "Problems occurred inside the server",
         "content": {
@@ -118,37 +178,53 @@ def update_wallet(update_wallet_data: WalletUpdateData):
             }
         }
     },
-    204: {
-        "description": "User have no wallets",
+    401: {
+        "description": "Authorization failure",
         "content": {
             "application/json": {
-                "example": {"detail": "no_content"}
+                "example": [{"detail": "no_authorization_header"},
+                            {"detail": "invalid_auth_scheme"},
+                            {"detail": "invalid_token"},
+                            {"detail": "expired_token"},
+                            {"detail": "unathorized_user_for_method"}
+                            ]
+
             }
         }
+    },
+    204: {
+        "description": "User have no wallets"
     },
     200: {
         "description": "List of objects with wallets details",
         "content": {
             "application/json": {
-                "example": [
-                    {
-                        "id": "1",
-                        "currency": "PLN",
-                        "value": "250.32",
-                        "user_id": "2"
-                    },
-                    {
-                        "id":"52",
-                        "currency": "USD",
-                        "value":"0.52",
-                        "user_id": "2"
-                    }
-                ]
+                "example": {
+                    "wallets": [
+                        {
+                            "id": "1",
+                            "currency": "PLN",
+                            "value": "250.32",
+                            "user_id": "2"
+                        },
+                        {
+                            "id": "52",
+                            "currency": "USD",
+                            "value": "0.52",
+                            "user_id": "2"
+                        }
+                    ]
                 }
             }
         }
-})
-def get_wallets(user_id):
+    }
+}, description="Returns a list of all wallets for user which id is given as parameter.")
+def get_wallets(user_id, request: Request):
+    auth_header = request.headers.get("Authorization")
+    jwt_payload = verify_user(auth_header)
+    if str(jwt_payload.get("id")) != user_id:
+        raise HTTPException(status_code=401, detail="unauthorized_user_for_method")
+
     user_data = user_detail_pb2.UserDetail(id=user_id)
 
     try:
@@ -159,9 +235,147 @@ def get_wallets(user_id):
         print("gRPC status code:", e.code())
         raise HTTPException(status_code=500, detail="internal_server_error")
 
-    if response is None:
-        raise HTTPException(status_code=400, detail="operation_failed")
-    elif response.wallets is None:
-        raise HTTPException(status_code=204, detail="no_content")
+    if len(response.wallets) == 0:
+        raise HTTPException(status_code=204)
     else:
-        return {"wallets": response.wallets}
+        return MessageToDict(response)
+
+
+@wallets.get("/wallet", responses={
+    500: {
+        "description": "Problems occurred inside the server",
+        "content": {
+            "application/json": {
+                "example": {"detail": "internal_server_error"}
+            }
+        }
+    },
+    400: {
+        "description": "Getting list of wallets failed",
+        "content": {
+            "application/json": {
+                "example": {"detail": "operation_failed"}
+            }
+        }
+    },
+    401: {
+        "description": "Authorization failure",
+        "content": {
+            "application/json": {
+                "example": [{"detail": "no_authorization_header"},
+                            {"detail": "invalid_auth_scheme"},
+                            {"detail": "invalid_token"},
+                            {"detail": "expired_token"},
+                            {"detail": "unauthorized_user_for_method"}
+                            ]
+
+            }
+        }
+    },
+    204: {
+        "description": "User have no wallets"
+    },
+    200: {
+        "description": "Details of specified wallet",
+        "content": {
+            "application/json": {
+                "example": {
+                    "id": "1",
+                    "currency": "PLN",
+                    "value": "250.32",
+                    "user_id": "2"
+                }
+            }
+        }
+    }
+}, description="Returns wallet details of specified id")
+def get_wallet_by_id(wallet_id, request: Request):
+    auth_header = request.headers.get("Authorization")
+    jwt_payload = verify_user(auth_header)
+
+    wallet_data = wallet_pb2.Wallet(id=wallet_id)
+
+    try:
+        stub = wallet_pb2_grpc.WalletsStub(channel)
+        response: wallet_pb2.Wallet = stub.getWallet(wallet_data)
+    except RpcError as e:
+        print("gRPC error details:", e.details())
+        print("gRPC status code:", e.code())
+        raise HTTPException(status_code=500, detail="internal_server_error")
+
+    if response.id == "":
+        raise HTTPException(status_code=204)
+
+    if str(jwt_payload.get("id")) != response.user_id:
+        raise HTTPException(status_code=401, detail="unauthorized_user_for_method")
+
+    return MessageToDict(response)
+
+
+@wallets.delete("/", responses={
+    500: {
+        "description": "Problems occurred inside the server",
+        "content": {
+            "application/json": {
+                "example": {"detail": "internal_server_error"}
+            }
+        }
+    },
+    400: {
+        "description": "Getting list of wallets failed",
+        "content": {
+            "application/json": {
+                "example": {"detail": "operation_failed"}
+            }
+        }
+    },
+    401: {
+        "description": "Authorization failure",
+        "content": {
+            "application/json": {
+                "example": [{"detail": "no_authorization_header"},
+                            {"detail": "invalid_auth_scheme"},
+                            {"detail": "invalid_token"},
+                            {"detail": "expired_token"},
+                            {"detail": "unauthorized_user_for_method"}
+                            ]
+
+            }
+        }
+    },
+    204: {
+        "description": "User have no wallets"
+    },
+    200: {
+        "description": "Details of removed wallet",
+        "content": {
+            "application/json": {
+                "example": {
+                    "id": "1",
+                    "currency": "PLN",
+                    "value": "250.32",
+                    "user_id": "2"
+                }
+            }
+        }
+    }
+}, description="Deletes wallet of specified id")
+def delete_wallet(wallet_id, request: Request):
+    try:
+        wallet_data = get_wallet_by_id(wallet_id=wallet_id, request=request)
+    except HTTPException as e:
+        raise e
+
+    try:
+        wallet_id_message = wallet_pb2.Wallet(id=wallet_data.get("id"))
+        stub = wallet_pb2_grpc.WalletsStub(channel)
+        response: wallet_pb2.Wallet = stub.deleteWallet(wallet_id_message)
+    except RpcError as e:
+        print("gRPC error details:", e.details())
+        print("gRPC status code:", e.code())
+        raise HTTPException(status_code=500, detail="internal_server_error")
+
+    if response.id == "":
+        raise HTTPException(status_code=204)
+
+    return MessageToDict(response)
